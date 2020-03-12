ecec84dfd749c0d8d49b96e5349fe766f5b85ede
Pradeep Vajrala <pradeepvajralaroyal2001@gmail.com>
2020-03-12 23:41:16 +0530

    Add files via upload

---
+++ lang.py
@@ -0,0 +1,375 @@
+w=print('1. For kids\n2. For fun\n3. For knowledge\n4. I am interested\n5. Improve myself\n6. For money\n7. Just want to learn one or for Data Analysis in the future')
+print('Select an option and enter it:')
+q=int(input())
+
+if q==1:
+    print('Learn Scratch')
+
+if q==2:
+    print('Have a brilliant idea/platform in mind?')
+    print('Y or N')
+    e=str(input())
+    if e=='Y':
+        print('Which of the following platform?\n1.Web\n2. Enterprise or Start-up\n3. Mobile\n4. 3D Gaming')
+        r=int(input())
+        if r==1:
+            print('Does your web-app provide info in real time like Twitter?')
+            print('Y or N')
+            t=str(input())
+            if t=='Y':
+                print('Learn JS')
+            else:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+        if r==2:
+            print('Do you want to learn something with huge potential?')
+            print('Y or N')
+            y=str(input())
+            if y=='Y':
+                print('Learn JS')
+            else:
+                print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                u=int(input())
+                if u==1:
+                      print('Learn Python')
+                elif u==2:
+                   print('Learn Ruby')
+                else:
+                   print('Learn PHP')
+        if r==3:
+            print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+            i=int(input())
+            if i==1:
+                print('Learn Java and explore other options too')
+            elif i==2:
+                print('Learn Swift')
+            elif i==3:
+                print('Learn C#')
+            else:
+                print('Learn Flutter but explore')
+        if r==4:
+            print('Learn C++')
+    if e=='N':
+        print('You want to learn things\n1. The easy way\n2. The best way\n3. Slightly harder way\n4. Really hard way')
+        o=int(input())
+        if o==1 or o==2:
+            print('Learn Python')
+        elif o==3:
+            print('Java or C')
+        else:
+            print('Learn C++')
+
+if q==3:
+    print('Have a brilliant idea/platform in mind?')
+    print('Y or N')
+    e=str(input())
+    if e=='Y':
+        print('Which of the following platform?\n1.Web\n2. Enterprise or Start-up\n3. Mobile\n4. 3D Gaming')
+        r=int(input())
+        if r==1:
+            print('Does your web-app provide info in real time like Twitter?')
+            print('Y or N')
+            t=str(input())
+            if t=='Y':
+                print('Learn JS')
+            else:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+        if r==2:
+            print('Do you want to learn something with huge potential?')
+            print('Y or N')
+            y=str(input())
+            if y=='Y':
+                print('Learn JS')
+            else:
+                print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                u=int(input())
+            if u==1:
+                print('Learn Python')
+            elif u==2:
+                print('Learn Ruby')
+            else:
+                print('Learn PHP')
+        if r==3:
+            print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+            i=int(input())
+            if i==1:
+                print('Learn Java and explore other options too')
+            elif i==2:
+                print('Learn Swift')
+            elif i==3:
+                print('Learn C#')
+            else:
+                print('Learn Flutter but explore')
+        if r==4:
+            print('Learn C++')
+    if e=='N':
+        print('You want to learn things\n1. The easy way\n2. The best way\n3. Slightly harder way\n4. Really hard way')
+        o=int(input())
+        if o==1 or o==2:
+            print('Learn Python')
+        elif o==3:
+            print('Java or C')
+        else:
+            print('Learn C++')
+
+if q==4:
+    print('Have a brilliant idea/platform in mind?')
+    print('Y or N')
+    e=str(input())
+    if e=='Y':
+        print('Which of the following platform?\n1.Web\n2. Enterprise or Start-up\n3. Mobile\n4. 3D Gaming')
+        r=int(input())
+        if r==1:
+            print('Does your web-app provide info in real time like Twitter?')
+            print('Y or N')
+            t=str(input())
+            if t=='Y':
+                print('Learn JS')
+            else:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+        if r==2:
+            print('Do you want to learn something with huge potential?')
+            print('Y or N')
+            y=str(input())
+            if y=='Y':
+                print('Learn JS')
+            else:
+                print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                u=int(input())
+                if u==1:
+                    print('Learn Python')
+                elif u==2:
+                      print('Learn Ruby')
+                else:
+                    print('Learn PHP')
+        if r==3:
+            print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+            i=int(input())
+            if i==1:
+                print('Learn Java and explore other options too')
+            elif i==2:
+                print('Learn Swift')
+            elif i==3:
+                print('Learn C#')
+            else:
+                print('Learn Flutter but explore')
+        if r==4:
+            print('Learn C++')           
+    if e=='N':
+        print('You want to learn things\n1. The easy way\n2. The best way\n3. Slightly harder way\n4. Really hard way')
+        o=int(input())
+        if o==1 or o==2:
+            print('Learn Python')
+        elif o==3:
+            print('Java or C')
+        else:
+            print('Learn C++')
+
+if q==5:
+    print('Have a brilliant idea/platform in mind?')
+    print('Y or N')
+    e=str(input())
+    if e=='Y':
+        print('Which of the following platform?\n1.Web\n2. Enterprise or Start-up\n3. Mobile\n4. 3D Gaming')
+        r=int(input())
+        if r==1:
+            print('Does your web-app provide info in real time like Twitter?')
+            print('Y or N')
+            t=str(input())
+            if t=='Y':
+                print('Learn JS')
+            else:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+        if r==2:
+            print('Do you want to learn something with huge potential?')
+            print('Y or N')
+            y=str(input())
+            if y=='Y':
+                print('Learn JS')
+            else:
+                print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                u=int(input())
+                if u==1:
+                    print('Learn Python')
+                elif u==2:
+                    print('Learn Ruby')
+                else:
+                    print('Learn PHP')
+        if r==3:
+            print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+            i=int(input())
+            if i==1:
+                print('Learn Java and explore other options too')
+            elif i==2:
+                print('Learn Swift')
+            elif i==3:
+                print('Learn C#')
+            else:
+                print('Learn Flutter but explore')
+        if r==4:
+            print('Learn C++')
+    if e=='N':
+        print('You want to learn things\n1. The easy way\n2. The best way\n3. Slightly harder way\n4. Really hard way')
+        o=int(input())
+        if o==1 or o==2:
+            print('Learn Python')
+        elif o==3:
+            print('Java or C')
+        else:
+            print('Learn C++')
+
+if q==6:
+    print('Do you have a start-up idea?\nY or N')
+    e=str(input())
+    if e=='Y':
+        print('Which of the following platform?\n1.Web\n2. Enterprise or Start-up\n3. Mobile\n4. 3D Gaming')
+        r=int(input())
+        if r==1:
+            print('Does your web-app provide info in real time like Twitter?')
+            print('Y or N')
+            t=str(input())
+            if t=='Y':
+                print('Learn JS')
+            else:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+        if r==2:
+            print('Do you want to learn something with huge potential?')
+            print('Y or N')
+            y=str(input())
+            if y=='Y':
+                print('Learn JS')
+            else:
+                print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                u=int(input())
+                if u==1:
+                    print('Learn Python')
+                elif u==2:
+                    print('Learn Ruby')
+                else:
+                    print('Learn PHP')
+        if r==3:
+            print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+            i=int(input())
+            if i==1:
+                print('Learn Java and explore other options too')
+            elif i==2:
+                print('Learn Swift')
+            elif i==3:
+                print('Learn C#')
+            else:
+                print('Learn Flutter but explore')
+        if r==4:
+            print('Learn C++')
+    print('For getting a job in: \n1. Gaming\n2. Enterprise or start-up\n3. Web\n4. Mobile\n5. Does\'nt matter\n6. I want to work for big tech companies')
+    a=int(input())
+    if a==1:
+        print('Learn C++')
+    if a==2:
+        print('Learn C# and Java')
+    if a==3:
+        print('Do you want to learn\n1. Front-end\n2. Back-end\n2.1. Work for start-ups\n2.2. Work for corporate')
+        s=float(input())
+        if s==1:
+            print('Learn JS')
+        if s==2:
+            if s==2.1:
+                print('Do you want to learn something with huge potential?')
+                print('Y or N')
+                y=str(input())
+                if y=='Y':
+                    print('Learn JS')
+                else:
+                    print('Which is your fav toy?\n1. Lego\n2. Play-dough\n3. Old toy')
+                    u=int(input())
+                    if u==1:
+                        print('Learn Python')
+                    elif u==2:
+                        print('Learn Ruby')
+                    else:
+                        print('Learn PHP')
+            if s==2.2:
+                print('Learn C# and Java')
+    if s==4:
+         print('Which OS?\n1. Android\n2. iOS\n3. Windows\n4. Cross-platform')
+         i=int(input())
+         if i==1:
+             print('Learn Java and explore other options too')
+         elif i==2:
+             print('Learn Swift')
+         elif i==3:
+             print('Learn C#')
+         else:
+             print('Learn Flutter but explore')
+    if s==5:
+        print('Learn Java')
+    if s==6:
+        print('For Google and Facebook\n Learn Python\nFor Apple\n Learn C and Swift\nFor Microsoft\n Learn C++')
+
+if q==7:
+    print('First learn Python then explore furthur')
+
+print('\nHappy Learning\n')
